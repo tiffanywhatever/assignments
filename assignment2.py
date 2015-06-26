a83aada3fc868eb470dbaec1154c7585af8073c4
tiffany <tiffanywhatever@gmail.com>
2015-06-25 23:32:27 -0400

    assignment2

--- assignment2.py
+++ assignment2.py
@@ -1,1 +1,27 @@
-# Assignment #2
+ # Assignment 2
+
+operators = {'+': 'plus',
+			'-': 'minus',
+			'/': 'divide by',
+			'*': 'times'}
+
+a_number = raw_input('Enter a number: ')
+operator = raw_input('Enter an operator: ')
+another_number = raw_input('Enter another number: ')
+
+# +, -, *, /
+a = int(a_number)
+b = int(another_number)
+
+if operator == '+':
+	print("%d %s %d equals %d" % (a, operators[operator], b, a + b))
+elif operator == '*':
+	print("%d %s %d equals %d" % (a, operators[operator], b, a * b)) 
+elif operator == '-':
+	print("%d %s %d equals %d" % (a, operators[operator], b, a - b))
+elif operator == '/':
+	print("%d %s %d equals %d" % (a,operators[operator], b, a / b))
+else:
+	print('operator not supported')
+
+
