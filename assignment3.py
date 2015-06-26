1d092dcd4813a40c8eae9cc12b0b3d056a587a2d
tiffany <tiffanywhatever@gmail.com>
2015-06-26 16:54:02 -0400

    assignmets3

---
+++ assignment3.py
@@ -0,0 +1,47 @@
+#Assignment 3
+
+number = raw_input('Enter a number: ')
+
+
+if number == '0':
+	print ("zero")
+elif number == '1':
+	print ("one")
+elif number == '2':
+	print ("two")
+elif number == '3':
+	print ("three")
+elif number == '4':
+	print ("four")
+elif number == '5':
+	print ("five")
+elif number == '6':
+	print ("six")
+elif number == '7':
+	print ("seven")
+elif number == '8':
+	print ("eight")
+elif number == '9':
+	print ("nine")
+elif number == '10':
+	print ("ten")
+elif number == '11':
+	print ("eleven")
+elif number == '12':
+	print ("twelve")
+elif number == '13':
+	print ("thirteen")
+elif number == '14':
+	print ("fourteen")
+elif number == '15':
+	print ("fifteen")
+elif number == '16':
+	print ("sixteen")
+elif number == '17':
+	print ("seventeen")
+elif number == '18':
+	print ("eighteen")
+elif number == '19':
+	print ("nineteen")
+else:
+	print ("Number is not supported.")	
